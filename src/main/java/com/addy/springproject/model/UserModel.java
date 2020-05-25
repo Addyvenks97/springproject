86136ae9b8ec30c3266b523cdefb3895434f4899
Addyvenka97 <aditya.venkataraman97@gmail.com>
2020-05-25 13:58:37 +0530

    Add controller to display a jsp

---
+++ src/main/java/com/addy/springproject/model/UserModel.java
@@ -0,0 +1,17 @@
+package com.addy.springproject.model;
+
+public class UserModel {
+
+    private String name;
+    private String email;
+    public UserModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/loginPage.jsp
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+</html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login page</title>
+</head>
+<body>
+
+
+<h1>Hi</h1>
+
+
+
+</body>
+</html>
